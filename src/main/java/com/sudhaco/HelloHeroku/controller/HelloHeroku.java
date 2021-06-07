fbdbd28ddfcb2fe17f360a13ab5c7470f9dfa063
Sudha SK <sskumar4@ncsu.edu>
2021-06-07 14:25:59 -0400

    first commit

---
+++ src/main/java/com/sudhaco/HelloHeroku/controller/HelloHeroku.java
@@ -0,0 +1,22 @@
+package com.sudhaco.helloheroku.controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class HelloHerokuController {
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String doHello() {
+        return "Hello, Heroku";
+    }
+    @RequestMapping(value = "/hi/{name}", method = RequestMethod.GET)
+    public String doHi(@PathVariable String name) {
+        return "What's happening, " + name;
+    }
+}
+
+
+
+
+
+
